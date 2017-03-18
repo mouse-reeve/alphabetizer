eee596d3f90a0bf15cacedf31f9ae9513c441539
Mouse Reeve <mousereeve@riseup.net>
2017-03-18 10:00:08 -0700

    Catch-all error handling so the script won't crash
    hopefully

--- tweeter.py
+++ tweeter.py
@@ -10,27 +10,30 @@ api = TwitterAPI(settings.API_KEY,
 
 variants = ['alphabetiz', 'alphabetis']
 endings = ['e', 'ing', 'ed', 'er', 'es']
-track = [v+e for v in variants for e in endings]
+track = ','.join(v+e for v in variants for e in endings)
 
-stream = api.request('statuses/filter', {'track': ','.join(track)})
+stream = api.request('statuses/filter', {'track': track})
 
 for item in stream.get_iterator():
-    if 'text' in item and not item['user']['screen_name'] == 'alphabotizer':
-        print '--- I found a tweet! ---'
-        text = item['text']
-        print text
-        tweet_id = item['id']
-        url = 'https://twitter.com/%s/status/%d' % (item['user']['screen_name'],
-                                                    tweet_id)
+    try:
+        if 'text' in item and not item['user']['screen_name'] == 'alphabotizer':
+            print '--- I found a tweet! ---'
+            text = item['text']
+            print text
+            tweet_id = item['id']
+            user = item['user']['screen_name']
+            url = 'https://twitter.com/%s/status/%d' % (user, tweet_id)
 
-        alphabetized = '%s %s' % (alphabetize(text), url)
-        print alphabetized
+            alphabetized = '%s %s' % (alphabetize(text), url)
+            print alphabetized
 
-        resp = api.request('statuses/update', {
-            'in_reply_to_status_id': tweet_id,
-            'status': alphabetized
-        })
-        print resp.response
-        if resp.status_code != 200:
-            print resp.text
-        print '\n\n\n'
+            resp = api.request('statuses/update', {
+                'in_reply_to_status_id': tweet_id,
+                'status': alphabetized
+            })
+            print resp.response
+            if resp.status_code != 200:
+                print resp.text
+            print '\n\n\n'
+    except Exception as oops:
+        print oops
