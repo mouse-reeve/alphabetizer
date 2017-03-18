4c4ff3c70db4ad3a453243ad114753a5cd267d6e
Mouse Reeve <mousereeve@riseup.net>
2017-03-17 17:45:18 -0700

    Tweeterbotter

---
+++ settings.py
@@ -0,0 +1,7 @@
+''' twitter API keys '''
+
+API_KEY = ''
+API_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+
---
+++ tweeter.py
@@ -0,0 +1,32 @@
+''' tweet out alphabetized versions of tweets '''
+from alphabetizer import alphabetize
+import settings
+from TwitterAPI import TwitterAPI
+
+api = TwitterAPI(settings.API_KEY,
+                 settings.API_SECRET,
+                 settings.ACCESS_TOKEN,
+                 settings.ACCESS_SECRET)
+
+stream = api.request('statuses/filter', {'track': 'alphabetize'})
+
+for item in stream.get_iterator():
+    if 'text' in item and not item['user']['screen_name'] == 'alphabotizer':
+        print '--- I found a tweet! ---'
+        text = item['text']
+        print text
+        tweet_id = item['id']
+        url = 'https://twitter.com/%s/status/%d' % (item['user']['screen_name'],
+                                                    tweet_id)
+
+        alphabetized = '%s %s' % (alphabetize(text), url)
+        print alphabetized
+
+        resp = api.request('statuses/update', {
+            'in_reply_to_status_id': tweet_id,
+            'status': alphabetized
+        })
+        print resp.response
+        if resp.status_code != 200:
+            print resp.text
+        print '\n\n\n'
